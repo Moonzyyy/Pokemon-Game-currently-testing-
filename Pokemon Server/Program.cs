fea2fb75e816f1a6706f7152ad04505df24d920a
Moonzyyy <47296443+Moonzyyy@users.noreply.github.com>
2021-06-25 15:38:12 +0100

    Add files via upload

---
+++ Pokemon Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Pokemon_Server
+{
+    class Program
+    {
+        private static bool isRunning = false;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Pokemon Game server";
+            isRunning = true;
+            Thread mainThread = new Thread(new ThreadStart(MainThread));
+            mainThread.Start();
+            Server.Start(10 , 24554);
+            
+        }
+
+        private static void MainThread()
+        {
+            Console.Write($"Main thread started. Running at {Constants.TICKS_PER_SEC} ticks per second");
+            DateTime _nextLoop = DateTime.Now;
+
+            while (isRunning)
+            {
+                while (_nextLoop < DateTime.Now)
+                {
+                    GameLogic.Update();
+
+                    _nextLoop = _nextLoop.AddMilliseconds(Constants.MS_PER_TICK);
+
+                    //This reduces CPU amount the server takes, was 13% reduced to close to 0
+                    if (_nextLoop > DateTime.Now)
+                    {
+                        Thread.Sleep(_nextLoop - DateTime.Now);
+                    }
+                }
+            }
+        }
+    }
+}
